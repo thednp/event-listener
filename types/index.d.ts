92f61febd8f75bd2d4ae9eda69b0871edb2a4c96
thednp <dnp.digital@gmail.com>
2022-02-03 17:06:44 +0200

    Update index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -6,4 +6,3 @@ import EventListener from 'event-listener.js/src/event-listener';
 
 export { default as EventRegistry } from "event-listener.js/src/registry";
 export { default as globalListener, addListener, removeListener } from "event-listener.js/src/globalListener";
-export { default as EventListener } from "event-listener.js/src/event-listener";
